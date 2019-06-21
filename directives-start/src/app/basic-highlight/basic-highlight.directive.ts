48af1a4fc032ed1d3b885ff3e35431cb56e1ebf9
Alfredo Barros <coomcs@gmail.com>
2019-06-21 19:28:36 -0300

    Better directives

--- directives-start/src/app/basic-highlight/basic-highlight.directive.ts
+++ directives-start/src/app/basic-highlight/basic-highlight.directive.ts
@@ -1,12 +1,14 @@
-import { Directive, ElementRef, OnInit } from "@angular/core";
+import { Directive, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
-	selector: '[appBasicHighlight]'
+selector: '[appBasicHighlight]'
 })
 export class BasicHighlightDirective implements OnInit {
-	constructor(private elementRef: ElementRef) { }
 
-	ngOnInit() {
-		this.elementRef.nativeElement.style.backgroundColor = 'green';
-	}
+  constructor( private elementRef: ElementRef ) { }
+
+  ngOnInit() {
+    this.elementRef.nativeElement.style.backgroundColor = 'green';
+    this.elementRef.nativeElement.style.color = 'white';
+  }
 }
