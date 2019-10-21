adb48223cf40670935c77c4ad1f76abf19894307
Alfredo Barros <alfredo.barros@incluit.com>
2019-10-21 16:42:45 -0300

    Completed services-start

--- services-start/src/app/account/account.component.ts
+++ services-start/src/app/account/account.component.ts
@@ -5,17 +5,22 @@ import { AccountsService } from '../accounts.service';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
+  styleUrls: ['./account.component.css']
+  // providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: { name: string, status: string };
   @Input() id: number;
 
-  constructor(private loggingService: LoggingService, private accountsService: AccountsService) { }
+  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {
+    this.accountsService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    );
+  }
 
   onSetTo(status: string) {
     this.accountsService.updateStatus(this.id, status);
-    this.loggingService.logStatusChange(status);
+    // this.loggingService.logStatusChange(status);
+    this.accountsService.statusUpdated.emit(status);
   }
 }
