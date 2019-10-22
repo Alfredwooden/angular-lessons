a8f0f0b084999784a0dfe8add216ed1142269ece
Alfredo Barros <alfredo.barros@incluit.com>
2019-10-22 15:43:00 -0300

    Added ingredients

--- angular-shopping-list/src/app/recipes/recipe.service.ts
+++ angular-shopping-list/src/app/recipes/recipe.service.ts
@@ -1,14 +1,30 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>()
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super tasty Schnitzel - Just awesome!',
+      'https://www.thespruceeats.com/thmb/LeyN-7W9T0KB2nl6pcuDZckHnjc=/4288x2848/filters:fill(auto,1)/wiener-schnitzel-recipe-1447089-Hero-5b587d6c46e0fb0071b0059d.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+
+    new Recipe(
+      'Big Fat Burguer',
+      'What else you need to say?',
+      'https://www.thespruceeats.com/thmb/EsAJdXE_40F6OqK1x8wzhHUXdtQ=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/luther-burger-doughnut-bacon-cheeseburger-4018810-hero-01-6fe92cf3005f4585bc2ff361a048b5f7.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
 
   getRecipes() {
